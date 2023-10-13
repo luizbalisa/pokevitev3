1280979688ca72bc04ce2f0417e0f1114d8bfc1a
Luiz Balisa <hidy63@gmail.com>
2023-10-13 10:40:00 -0300

    feat: inicia estudo v3

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const nomeAluno = ref('Joacao');
+const nota1 = ref(10);
+const nota2 = ref(10);
+const media = computed(()=> {nota1.value + nota2.value / 2});
+
+const rendaFamiliar = ref(2000)
+
+const bolsista = computed(()=> rendaFamiliar.value <= 2000);
+ 
+</script>
+
+<template>
+  <div class="col">
+  <h2>composition</h2>
+  <h3>{{nomeAluno}}</h3> 
+</div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
