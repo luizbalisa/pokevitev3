bdc400634304ba3676f7ce2673c8d69acd930dd4
Luiz Balisa <hidy63@gmail.com>
2023-10-27 16:49:03 -0300

    fix: vues ajuste

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,51 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script></script>
 
 <template>
-  <HelloWorld />
+  <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">
+        <img src="./assets/icon.svg" alt="" height="40" class="d-inline-block align-text-center" />
+        PokeVite
+      </router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- CONTEÚDO DA APLICAÇÃO -->
+  <div class="main">
+    <router-view></router-view>
+  </div>
+  <!-- -->
+
+  <footer class="bg-primary text-light py-3 text-center">
+    <div>
+      <span>&copy {{new Date().getFullYear()}}</span>
+    </div>
+  </footer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  background-color: #343a40;
 }
 </style>
